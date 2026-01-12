debaae07d44e66854b8db54b5dbd5ad43ffb3fa1
Bardoctorus <ian.m.buckley@gmail.com>
2026-01-12 13:50:31 +0100

    added rest of the advertising stuff on top of previous

--- src/main.c
+++ src/main.c
@@ -5,21 +5,26 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
+ //general zephyr includes
 #include <zephyr/kernel.h>
 #include <zephyr/device.h>
 #include <zephyr/devicetree.h>
 #include <zephyr/drivers/sensor.h>
 #include <zephyr/drivers/sensor_data_types.h>
 #include <zephyr/rtio/rtio.h>
+
+
+// logging and printing includes
 #include <zephyr/dsp/print_format.h>
-
-
 #include <zephyr/logging/log.h>
 
 
-
+// bluetooth includes
 #include <zephyr/bluetooth/bluetooth.h>
+#include <zephyr/bluetooth/conn.h>
 #include <zephyr/bluetooth/gap.h>
+#include <zephyr/bluetooth/uuid.h>
+#include <zephyr/bluetooth/addr.h>
 
 //nordic buttons and leds for testing 
 #include <dk_buttons_and_leds.h>
@@ -35,8 +40,14 @@ typedef struct adv_mfg_data {
 	uint16_t number_press;
 } adv_mfg_data_type;
 
+// dummy url data
+static unsigned char url_data[] = { 0x17, '/', '/', 'x', 'k', 'c', 'd', '.', 'c',
+				    'o',  'm' };
+
+static struct k_work adv_work;
+
 static const struct bt_le_adv_param *adv_param = 
-	BT_LE_ADV_PARAM(BT_LE_ADV_OPT_NONE, // no avertising options selected
+	BT_LE_ADV_PARAM((BT_LE_ADV_OPT_CONN | BT_LE_ADV_OPT_USE_IDENTITY), // advertisin options
 		800, //min advertisins interval 500ms (800 *0.625)
 		801, //max adv time 500.625ms (801 *0.625)
 		NULL // undirected advertising
@@ -52,22 +63,53 @@ LOG_MODULE_REGISTER(tempsound_logger, LOG_LEVEL_INF);
 
 static const struct bt_data ad[] = {
 	//advertising flags
-	BT_DATA_BYTES(BT_DATA_FLAGS, BT_LE_AD_NO_BREDR),
+	BT_DATA_BYTES(BT_DATA_FLAGS, (BT_LE_AD_GENERAL | BT_LE_AD_NO_BREDR)),
 	//advertising packet data
 	BT_DATA(BT_DATA_NAME_COMPLETE, DEVICE_NAME, DEVICE_NAME_LEN),
 	//manufacturer data 
 	BT_DATA(BT_DATA_MANUFACTURER_DATA, (unsigned char*) &adv_mfg_data, sizeof(adv_mfg_data)),
 };
 
-
-// dummy url data
-static unsigned char url_data[] = { 0x17, '/', '/', 'x', 'k', 'c', 'd', '.', 'c',
-				    'o',  'm' };
-
 static const struct bt_data sd[] ={
 	//scan response packet - dummy url
 	BT_DATA(BT_DATA_URI, url_data,sizeof(url_data)),
-};			
+
+	// uuid of lbs service : https://docs.nordicsemi.com/bundle/ncs-latest/page/nrf/libraries/bluetooth/services/lbs.html#service-uuid
+	BT_DATA_BYTES(BT_DATA_UUID128_ALL, BT_UUID_128_ENCODE(0x00001523, 0x1212, 0xefde, 0x1523, 0x785feabcd123)),
+
+};	
+
+
+
+static void adv_work_handler(struct k_work *work)
+{
+	int err = bt_le_adv_start(adv_param, ad, ARRAY_SIZE(ad), sd, ARRAY_SIZE(sd));
+
+	if (err) {
+		printk("Advertising failed to start (err %d)\n", err);
+		return;
+	}
+
+	printk("Advertising successfully started\n");
+}
+static void advertising_start(void)
+{
+	k_work_submit(&adv_work);
+}
+static void recycled_cb(void)
+{
+	printk("Connection object available from previous conn. Disconnect is complete!\n");
+	advertising_start();
+}
+
+BT_CONN_CB_DEFINE(conn_callbacks) = {
+	.recycled = recycled_cb,
+};
+
+
+
+
+		
 
 static void button_changed(uint32_t button_state, uint32_t has_changed){
 	if (has_changed & button_state & USER_BUTTON){
@@ -137,6 +179,18 @@ int main(void)
 		return -1;
 	}
 
+	// set static address
+    bt_addr_le_t addr;
+
+	err = bt_addr_le_from_str("FF:EE:DD:CC:BB:AA", "random", &addr);
+	if (err) {
+        printk("Invalid BT address (err %d)\n", err);
+    }
+
+    err = bt_id_create(&addr, NULL);
+    if (err < 0) {
+        printk("Creating new ID failed (err %d)\n", err);
+    }
 	// cheking bme device is all the things
 	const struct device *dev = check_bme280_device();
 	if (dev == NULL) {
@@ -151,11 +205,14 @@ int main(void)
 	}
 	LOG_INF("Bluetooth Started");
 
-	err = bt_le_adv_start(BT_LE_ADV_NCONN, ad, ARRAY_SIZE(ad), sd, ARRAY_SIZE(sd));
-	if(err){
-		LOG_ERR("advertising did not start- error: %d", err);
-		return -1;
-	}
+	// err = bt_le_adv_start(BT_LE_ADV_NCONN, ad, ARRAY_SIZE(ad), sd, ARRAY_SIZE(sd));
+	// if(err){
+	// 	LOG_ERR("advertising did not start- error: %d", err);
+	// 	return -1;
+	// }
+
+	k_work_init(&adv_work, adv_work_handler);
+	advertising_start();
 
 	LOG_INF("Advertising Started");
 
